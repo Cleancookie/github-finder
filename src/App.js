a69f88ea14b7423a99862c31409b9c69b7a66f9d
Alex Law <pokemon999101@hotmail.co.uk>
2020-02-06 22:53:01 +0000

    ✨ Make Users comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
